28f27d93162d44c7b5e7aff3553894b7e9d530bd
Sudhansu Dash <sdash@lis-stage.agron.iastate.edu>
2015-12-01 18:19:54 -0600

    Modifications after GRIN website URL changes

--- grinconnect_page.tpl.php
+++ grinconnect_page.tpl.php
@@ -1,7 +1,7 @@
 <!-- Title and guide text  -->
 <div>
   <h1>GRIN Connect</h1>
-  <span>Connect to USDA <a href="http://www.ars-grin.gov/npgs/" target="_blank">GRIN (NPGS)</a> germplasm resources with a <b>GRIN accession number</b> or <b>cultivar name</b>.</span>
+  <span>Connect to USDA <a href="http://www.ars-grin.gov/npgs/" target="_blank">GRIN (NPGS)</a> germplasm resources with a <b>GRIN accession number</b><!-- or <b>cultivar name</b>-->.</span>
   <br/>
 </div>
 
@@ -9,7 +9,7 @@
 <div>
 <form  method="get">
   <br/>
-  Enter GRIN Accession Number OR a cultivar name? <br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+  Enter GRIN Accession Number<!-- OR a cultivar name-->? <br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
   
   <input type="text" id="grin_acc_no" name="grin_acc_no" required value="<?php print $_GET['grin_acc_no']; ?>" >
   &nbsp;&nbsp;&nbsp;&nbsp;<input type="submit" value="SUBMIT">
@@ -23,8 +23,9 @@
 <div>
   <hr/>
   <fieldset  style="background: #E0E0E0;">
-    Your search will return a web page directly from <b>GRIN</b>. You may need to scroll down within the GRIN page below to see all of the information. In that page, click the <b>Observations</b> link to see <b>evaluation data</b> if it exists. 
+    Your search will return a web page directly from <b>GRIN</b>. You may need to scroll down within the GRIN page below to see all of the information. In that page, click the <b>'Detailed Accession Observation Page'</b> link to see <b>evaluation data</b> if it exists. 
   </fieldset>
+
 </div>
 
 <!-- Iframe GRIN  -->
